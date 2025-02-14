64387183c78ed4aeb8927910d50c800cbe625913
cimeries <cimeries.devel@gmail.com>
2025-02-13 23:25:28 -0500

    fix

--- src/utils/snapshot.tsx
+++ src/utils/snapshot.tsx
@@ -2,11 +2,11 @@ import { collection, doc, Firestore, onSnapshot } from "firebase/firestore";
 import { Dispatch, SetStateAction } from "react";
 import { DataDocument } from "../context/context";
 
-export const snapshotDocument = (db: Firestore, id: string, setLoading: Dispatch<SetStateAction<boolean>>, setDataDocument:Dispatch<SetStateAction<any>>) => {
-  const docRef = doc(db, 'pi', id); // Reemplaza con el nombre de tu colección y el ID del documento
+export const snapshotDocument = (db: Firestore, id: string, setLoading: Dispatch<SetStateAction<boolean>>, setDataDocument:Dispatch<SetStateAction<DataDocument>>) => {
+  const docRef = doc(db, 'pi', id);
   const unsubscribe = onSnapshot(docRef, (doc) => {
     if (doc.exists()) {
-      setDataDocument(doc.data())
+      setDataDocument(doc.data() as DataDocument)
       setLoading(true)
     } else {
       console.log("No such document!");
@@ -15,12 +15,11 @@ export const snapshotDocument = (db: Firestore, id: string, setLoading: Dispatch
     console.error("Error al escuchar cambios:", error);
   });
 
-  // Guarda la función unsubscribe para poder dejar de escuchar los cambios cuando ya no sean necesarios
   return unsubscribe;
 };
 
 export const snapshotCollection = (db: Firestore, id: string, setDataCollection:Dispatch<SetStateAction<DataDocument[]>>) => {
-  const collectionRef = collection(db, id); // Reemplaza con el nombre de tu colección
+  const collectionRef = collection(db, id);
   const unsubscribe = onSnapshot(collectionRef, (snapshot) => {
     const data = snapshot.docs.map((doc) => doc.data() as DataDocument);
     setDataCollection(data);
