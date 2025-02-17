f901a165f45a3a33ca2a905fa555d67da1517083
cimeries <cimeries.devel@gmail.com>
2025-02-17 01:27:25 -0500

    fix files

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 1150,
+    chunkSizeWarningLimit: 2230,
   },
 });
